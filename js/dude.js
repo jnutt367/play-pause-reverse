ca1728307c5c0dd35a1135b2e065a94fe4254328
Jason Nutt <jnutt367@gmail.com>
2021-04-14 16:09:17 -0400

    commit for lunch break

--- js/dude.js
+++ js/dude.js
@@ -1,4 +1,4 @@
-var dudeGo = gsap.fromTo(".dude", { x: 400, opacity: "0" }, { duration: 3, x: 550, y: 200, marginLeft: "100", rotate: "360", scale: "3", ease: "bounce.inOut", opacity: "1", paused: true });
+var dudeGo = gsap.fromTo(".dude", { x: 400, opacity: "0" }, { duration: 3, x: 245, y: 190, marginLeft: "100", rotate: "360", scale: "3", width: "146", border: "2px inset green", ease: "bounce.inOut", zIndex: "0", opacity: ".5", paused: true });
 
 document.getElementById("play").onclick = () => dudeGo.play();
 
@@ -7,7 +7,7 @@ document.getElementById("pause").onclick = () => dudeGo.pause();
 document.getElementById("rewind").onclick = () => dudeGo.reverse();
 document.getElementById("restart").onclick = () => dudeGo.restart();
 
-gsap.to("button", { padding: "10", color: "black", backgroundColor: "green", borderRadius: "15px", fontFamily: "fantasy", fontWeight: "bold" });
+gsap.to("button", { padding: "10", color: "black", backgroundColor: "green", zIndex: "1", borderRadius: "15px", fontFamily: "fantasy", fontWeight: "bold" });
 gsap.to("a", { backgroundColor: "green", color: "white" });
 
-gsap.fromTo(".text", { x: 10, y: -10, opacity: "0" }, { duration: 2, x: 200, y: -10, ease: "back(6)", ease: "elastic", backgroundColor: "grey", color: "white", padding: "20", width: "400", borderRadius: "15px", fontSize: "1.5em", opacity: "1", fontFamily: "fantasy" });
+gsap.fromTo(".text", { x: 10, y: -10, opacity: "0" }, { duration: 2, x: 200, y: -10, ease: "back(6)", ease: "elastic", backgroundColor: "grey", color: "white", padding: "20", width: "400", fontSize: "1.5em", opacity: "1", fontFamily: "fantasy" });
--- js/gsap.js
+++ js/gsap.js
@@ -1,4 +1,4 @@
-var wizRight = gsap.fromTo(".wizard", { x: 400, opacity: "0" }, { duration: 3, x: 500, y: 200, scale: "2", ease: "circ.inOut", opacity: "1", paused: true });
+var wizRight = gsap.fromTo(".wizard", { x: 400, opacity: "0" }, { duration: 3, x: 300, y: 200, borderRadius: "15px", scale: "2", ease: "circ.inOut", opacity: "1", paused: true });
 
 
 
@@ -11,5 +11,5 @@ document.getElementById("restart").onclick = () => wizRight.restart();
 
 gsap.to("button", { padding: "10", color: "orange", backgroundColor: "black", borderRadius: "15px" });
 gsap.to("a", { backgroundColor: "orange", color: "white" });
-
-gsap.fromTo(".text", { x: 10, y: -10, opacity: "0" }, { duration: 2, x: 200, y: -10, ease: "elastic", backgroundColor: "orange", color: "white", padding: "20", width: "400", borderRadius: "15px", fontSize: "1.5em", opacity: "1", fontFamily: "fantasy" });
+gsap.to("body", { duration: 4, backgroundColor: "beige" });
+gsap.fromTo(".text", { x: 10, y: -10, opacity: "0" }, { duration: 2, x: 100, y: -10, ease: "elastic", backgroundColor: "orange", color: "white", padding: "20", width: "400", borderRadius: "15px", fontSize: "1.5em", opacity: "1", fontFamily: "fantasy" });
