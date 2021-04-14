8ec5c291caaec8a2d4a951b0757ad171d3bd040b
Jason Nutt <jnutt367@gmail.com>
2021-04-14 12:33:48 -0400

    commit js and html for third animation

---
+++ js/giant.js
@@ -0,0 +1,10 @@
+var giantGo = gsap.fromTo(".giant", { x: 400, opacity: "0" }, { duration: 3, x: 550, y: 400, scale: "3", height: "400", ease: "steps(12)", opacity: "1", paused: true });
+
+document.getElementById("play").onclick = () => giantGo.play();
+
+
+document.getElementById("pause").onclick = () => giantGo.pause();
+document.getElementById("rewind").onclick = () => giantGo.reverse();
+document.getElementById("restart").onclick = () => giantGo.restart();
+
+gsap.to("button", { padding: "10", color: "white", backgroundColor: "purple", borderRadius: "15px", fontFamily: "fantasy", fontWeight: "bold" });
